1b95955a8abaf13523d4a30b97e4c67c890336b6
Sanjog Aale <sanjogm940@gmail.com>
2026-06-14 17:49:55 +0545

    tailwindcss

--- src/pages/about.page.jsx
+++ src/pages/about.page.jsx
@@ -2,9 +2,10 @@ import Navbar from "../components/navbar";
 
 const AboutPage = () => {
   return (
-    <main className="page">
+    <main className="h-screen border border-red-400 flex justify-center items-center gap-10">
       <Navbar/>
-      <h1>About Page</h1>
+      <p className="text-[13px] text-red-200 border border-purple-50 rounded-sm px-4 py-2 hover:bg-blue-400 hover:text-white">About Page</p>
+      <p className=""></p>
     </main>
   )
 }
